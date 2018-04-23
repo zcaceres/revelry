1d37bc42c477b9552bce870d783cf311bd08e971
zc <zachcaceres@gmail.com>
2018-04-22 22:32:24 -0400

    new routers

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.get('/', function(req, res) {
 })
 
 app.use('/music', musicRouter);
-
 app.use('/unreal', unrealRouter);
+app.use('/mario', marioRouter);
+app.use('/misc', miscRouter);
 
 app.listen(3000, () => console.log('Listening :3000!'))
--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,11 @@
 const music = require('music');
 const unreal = require('unreal');
+const mario = require('mario');
+const misc = require('misc');
 
 module.exports = {
   music,
-  unreal
+  unreal,
+  mario,
+  misc
 }
